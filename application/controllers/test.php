650222302c594b803dc9abf988735d89f990d0ae
GoranAviani <goranaviani@gmail.com>
2018-10-03 22:16:48 +0200

    test.php controller

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -5,3 +5,17 @@
  * Date: 10/3/2018
  * Time: 10:02 PM
  */
+
+class Test extends CI_Controller
+{
+	public function index()
+	{
+		echo 'Hello World!';
+	}
+
+	function phpinfo()
+	{
+		phpinfo();
+	}
+}
+?>
