54c7fb2997b9568baee282d394dd75ee060403ca
GoranAviani <goranaviani@gmail.com>
2018-10-14 17:06:44 +0200

    first view file added, footer view added

---
+++ application/views/test.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang = "en">
+
+   <head>
+      <meta charset = "utf-8">
+      <title>Example of CodeIgniter View</title>
+   </head>
+
+   	<body>
+		This is an example of CodeIgniter View
+	</body>
+
+</html>
