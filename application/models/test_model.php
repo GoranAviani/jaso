f48a3de33704eec947284a7b96de0ae171cf89b5
GoranAviani <goranaviani@gmail.com>
2018-10-14 17:56:22 +0200

    first model

---
+++ application/models/test_model.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Goran
+ * Date: 10/14/2018
+ * Time: 5:50 PM
+ */
+
+Class Test_model extends CI_Model {
+
+	Public function insert_something() {
+		//
+	}
+}
+?>
